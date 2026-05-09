83b5c40d5e7f5adec4f3b99615d7cb72e5db5ef2
Nikolay Nekrasov <nnnekrasov@edu.hse.ru>
2026-05-09 13:02:15 +0000

    Add ffn

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,10 +1,12 @@
 from src.model.layer import Layer
 from src.model.embedding import InvertedDataEmbedding
+from src.model.ffn import FeedForward
 from src.model.transformer import Transformer
 
 
 __all__ = [
     "Layer",
     "InvertedDataEmbedding",
+    "FeedForward",
     "Transformer",
 ]
